031533cfbe6ec364babf35685affb014c32a7284
Abdul khalique <89987008+Khalique48@users.noreply.github.com>
2024-03-21 12:00:00 +0530

    Create SignUp api Route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+// created  a new user with hashed password
+export const signup = async (req, res) => {
+ const { username, email, password } = req.body;
+ const hashedPassword = bcryptjs.hashSync(password, 10)
+ const newUser = new User({ username, email, password: hashedPassword });
+ try{
+    res.status(201).json("User Created Successfully") 
+    await newUser.save()
+ } catch (error){
+  res.status(500).json(error.message);
+ }
+};
